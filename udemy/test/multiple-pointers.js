bf9532ac52b15b41d9870bcffeff666d526c1f26
Sonky <sonky740@gmail.com>
2022-09-22 21:35:24 +0900

    multiple-pointers, anagram

---
+++ udemy/test/multiple-pointers.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+solution([1, 2, 2, 5, 7, 7, 99]); // 5
+solution([1,1,1,1,1,2]) // 2
+solution([1,2,3,4,4,4,7,7,12,12,13]) // 7
+solution([]) // 0
+solution([-2,-1,-1,0,1]) // 4
---
+++ udemy/test/two-pointers.js
@@ -0,0 +1,22 @@
+// sumZero 문제배열에서 첫번째로 합이 0이 되는 쌍을 찾는 함수.
+function solution() {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+// solution([-3,-2,-1,0,1,2,3]) // [-3,3]
+// solution([-2,0,1,3]) // undefined
+// solution([1,2,3]) // undefined
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
