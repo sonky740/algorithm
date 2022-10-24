ee9fc9433ee3016295df0ff9ae77f6e5b4164c5e
Sonky <sonky740@gmail.com>
2022-10-25 00:02:28 +0900

    합병 정렬 2, 빅오복잡도

--- udemy/sort/merge.js
+++ udemy/sort/merge.js
@@ -23,3 +23,13 @@ function mergeSort(arr1, arr2) {
 }
 
 mergeSort([1, 10, 50], [2, 14, 99, 100]);
+
+function mergeSortRecursion(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSortRecursion(arr.slice(0, mid));
+  let right = mergeSortRecursion(arr.slice(mid));
+  return mergeSort(left, right);
+}
+
+mergeSortRecursion([10, 24, 76, 73, 72, 1, 9]);
