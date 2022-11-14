e362d1bfac2cdd0acd613089519ac2a636edb1f0
손기연 <sonky740@ex-em.com>
2022-11-14 15:46:25 +0900

    프로그래머스 알고리즘 링크 주석 추가

--- programmers/Lv_0/옹알이.js
+++ programmers/Lv_0/옹알이.js
@@ -1,4 +1,5 @@
 // 옹알이 lv.0
+// https://school.programmers.co.kr/learn/courses/30/lessons/120956
 // 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
 // 입출력 예 #1
 // ["aya", "yee", "u", "maa", "wyeoo"]에서 발음할 수 있는 것은 "aya"뿐입니다. 따라서 1을 return합니다.
