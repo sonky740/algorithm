e10b2ee959d8d4f29bcb06aa23385ead9081b1a7
손기연 <sonky740@ex-em.com>
2022-11-22 12:08:30 +0900

    프로그래머스: 문자열 밀기, 로또 최고 순위와 최저 순위, 올바른 괄호, 최솟값 만들기

---
+++ programmers/Lv_2/올바른괄호.js
@@ -0,0 +1,28 @@
+// 올바른 괄호 lv.2
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+// "()()" 또는 "(())()" 는 올바른 괄호입니다.
+// ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+// '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+// 문자열 s의 길이 : 100,000 이하의 자연수
+// 문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+function solution(s) {
+  let answer = true;
+  let cnt = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') cnt++;
+    else cnt--;
+    if (cnt < 0) {
+      answer = false;
+      break;
+    }
+  }
+  if (cnt !== 0) answer = false;
+
+  return answer;
+}
+solution('()()'); // true
+solution('(())()'); // true
+solution(')()('); // false
+solution('(()('); // false
+solution('(())(((())(())(())))((()))'); // true
