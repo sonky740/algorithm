0805b5a22070ec8351a91f27197378580e418277
Sonky <sonky740@gmail.com>
2022-12-24 00:42:39 +0900

    그래프 기본

---
+++ udemy/Graph/graph.js
@@ -0,0 +1,32 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+}
+
+const g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('Dallas');
+g.addVertex('Aspen');
+
+g.addEdge('Tokyo', 'Dallas');
+g.addEdge('Tokyo', 'Aspen');
+
+g.removeEdge('Tokyo', 'Aspen');
+
+console.log(g.adjacencyList);
