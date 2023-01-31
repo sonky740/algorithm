c3c2e7465b1db197ed324581fa62cf1274f8ca24
손기연 <sonky740@ex-em.com>
2023-01-31 15:35:45 +0900

    프로그래머스: 뒤에 있는 큰 수 찾기 수정

--- programmers/Lv_2/뒤에있는큰수찾기.js
+++ programmers/Lv_2/뒤에있는큰수찾기.js
@@ -5,16 +5,13 @@
 // 1 ≤ numbers[i] ≤ 1,000,000
 function solution(numbers) {
   const stack = [];
-  const answer = new Array(numbers.length).fill(0);
+  const answer = new Array(numbers.length).fill(-1);
 
   for (let i = 0; i < numbers.length; i++) {
     while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
       answer[stack.pop()] = numbers[i];
     }
     stack.push(i);
-  }
-  while (stack.length) {
-    answer[stack.pop()] = -1;
   }
 
   return answer;
